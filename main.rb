59c501ce671a334bb4516fa36429869871dbfa95
Zack Norton <160171391+grizztyrsen@users.noreply.github.com>
2024-05-17 17:40:12 -0500

    removed test input, substrings complete

--- main.rb
+++ main.rb
@@ -4,14 +4,9 @@ def substrings(word, dictionary)
 
   #iterates through each substring in the dictionary
   dictionary.each_with_object(Hash.new(0)) do |substring, occurrences|
-    #keeps count of how often each substring occurs in the dictionary
+    #keeps count of how often each substring occurs in the dictionary before storing it in the hash
     occurrence = word.scan(/#{substring.downcase}/).length
     #stores the substring and number of occurrences in the hash unless the substring does not occur
     occurrences[substring] += occurrence unless occurrence.zero?
   end
 end
-
-word = "Howdy partner, sit down! How's it going?"
-dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i", "low", "own", "part", "partner", "sit"]
-
-puts substrings(word, dictionary)
